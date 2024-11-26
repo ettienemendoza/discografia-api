7fda388269dad7fa750143f03f16eaada540ac79
ettienemendoza <ettiene.andre@gmail.com>
2024-11-25 21:49:44 -0300

    Actualización en IDiscoRepository

--- src/main/java/org/iplacex/proyectos/discografia/discos/IDiscoRepository.java
+++ src/main/java/org/iplacex/proyectos/discografia/discos/IDiscoRepository.java
@@ -3,13 +3,13 @@ package org.iplacex.proyectos.discografia.discos;
 
 import java.util.List;
 
-import org.iplacex.proyectos.discografia.artistas.Artistas;
+import org.iplacex.proyectos.discografia.artistas.Artista;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 
 public interface IDiscoRepository extends MongoRepository <Disco,String> {
     @Query("{ 'idArtista' : ?0 }")
-    Artistas findArtistasByid (String id) throws Error;
+    Artista findArtistasByid (String id) throws Error;
 
     List<Disco> findDiscosByIdArtista(String idArtista);
 
